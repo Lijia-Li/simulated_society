80a01926fb07c100e31bb365482aff14c9ebe8fb
Unknown <lil@oxy.edu>
2018-06-12 18:54:58 -0700

    add environment file

---
+++ environment.py
@@ -0,0 +1,29 @@
+"""This file contains environment functions that are used in the simulations."""
+
+def hazard():
+    """simulate the environment's hazards"""
+    return
+
+
+def social():
+    """social life simulation for men"""
+    return
+
+
+def resource():
+    # aggriculture than hunting
+    # seansonal stuff comes in
+    # plant -> grow -> harvest
+    # multiple resources
+        # flax/wool(from sheep/goat)  --> clothing (periodic attention)
+        # wheat (periodic attention)
+        # olives
+        # goat (no need too much attentions) *
+        # chickens *
+        # bee
+        # herbs
+        # water *
+        # salt
+        # dogs * (scare off strangers)
+
+    return
